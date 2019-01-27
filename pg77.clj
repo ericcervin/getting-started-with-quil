c328a95d9087b6512a7032fbd8b2a0ddefe0d1f2
ericcervin <ericcervin@gmail.com>
2019-01-27 16:56:39 -0500

    fixed some ns expressions

---
+++ pg77.clj
@@ -0,0 +1,18 @@
+(ns getting-started-with-quil.pg77
+  (:require [quil.core :as q]))
+
+(defn setup [])
+
+(defn draw []
+  (q/translate (q/mouse-x) (q/mouse-y))
+  (q/rect 0 0 30 30)
+  (q/translate 35 10)
+  (q/rect 0 0 15 15))
+
+(q/defsketch practice
+  :size [120 120]
+  :setup setup
+  :draw draw
+  :features [:keep-on-top])
+
+
